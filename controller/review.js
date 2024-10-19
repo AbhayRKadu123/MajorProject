e52025a53e9766d834fcc68aaf0a2fb46aa1f7d7
abhay rajesh kadu <abhaykadu2201@gmail.com>
2024-10-19 09:38:26 +0530

    fixing review error

--- controller/review.js
+++ controller/review.js
@@ -2,15 +2,17 @@ const Review=require("../Models/reviews");
 const Listing = require('../Models/listing.js');
 module.exports.CreateReview=async(req,res)=>{
    
-   console.log(req.params.id)
+   console.log('this is review'+req.params.id)
     var list=await Listing.findById(req.params.id)
     var NewReview=new Review(req.body.review);
     NewReview.author=req.user._id;
+   console.log('this is review'+NewReview.author)
+
     await NewReview.save();
     await list.reviews.push(NewReview);
     await list.save();
     req.flash('success', 'Review added !');
-    res.redirect(`http://localhost:8080/listings/${req.params.id}`)
+    res.redirect(`/listings/${req.params.id}`)
    
 
 };
@@ -21,6 +23,6 @@ module.exports.DeleteReview=async(req,res)=>{
     var r=await Listing.findByIdAndUpdate(req.params.id,{$pull:{reviews:req.params.id2}});
     req.flash('success', 'Review deleted Successfully!');
   
-    res.redirect(`http://localhost:8080/listings/${req.params.id}`)
+    res.redirect(`/listings/${req.params.id}`)
 
 }
